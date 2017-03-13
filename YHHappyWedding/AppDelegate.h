019359b2f3b2660d62ba1a971e88057c295abe99
YuheCui <cuiyuhe0920@126.com>
2017-03-13 20:56:06 +0800

    Initial Commit

---
+++ YHHappyWedding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHHappyWedding
+//
+//  Created by Cui yuhe on 17/3/13.
+//  Copyright © 2017年 Cui yuhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHHappyWedding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YHHappyWedding
+//
+//  Created by Cui yuhe on 17/3/13.
+//  Copyright © 2017年 Cui yuhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
